3f1d3306e7d1d5397b9161d00d3b6bc303295117
betaCode-lab <adriangpe5666@gmail.com>
2023-09-12 17:05:08 -0700

    Dbz module added

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/Character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  @Input() public characters: Character[] = [];
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string):void {
+
+    if(!id) return;
+
+    this.onDelete.emit(id);
+  }
+}
---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/Character.interface';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable({providedIn: 'root'})
+export class DbzService {
+
+  public characters: Character[] = [
+    {
+      id: uuidv4(),
+      name: 'Vegeta',
+      power: 7500
+    },
+    {
+      id: uuidv4(),
+      name: 'Kid Boo',
+      power: 234000
+    },
+    {
+      id: uuidv4(),
+      name: 'Trunks',
+      power: 50000
+    }
+  ];
+
+  constructor() { }
+
+  addCharacter( character:Character ):void {
+
+    const newCharacter:Character = {id: uuidv4(), ...character};
+
+    this.characters = [...this.characters, character];
+  }
+
+  deleteCharacterById( id:string ) {
+    this.characters = this.characters.filter( character => character.id !== id);
+  }
+}
